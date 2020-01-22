5dbeec48656580cdb8b25a730d772667ad16aeb6
Xu <Zhenghao.Xu@connext.com.cn>
2020-01-22 14:03:32 +0800

    add

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"os/exec"
 	"test/zhlog"
 )
 
@@ -26,16 +28,65 @@ func Init() {
 }
 
 func main() {
-	Init()
-	if cmd.UUID == "" || cmd.File == "" || cmd.CldType == "" || cmd.CldID == "" {
-		fmt.Println("以下命令行参数必填，且信息均为真实有效")
-		flag.PrintDefaults()
+	// Init()
+	// if cmd.UUID == "" || cmd.File == "" || cmd.CldType == "" || cmd.CldID == "" {
+	// 	fmt.Println("以下命令行参数必填，且信息均为真实有效")
+	// 	flag.PrintDefaults()
+	// 	return
+	// }
+	// fmt.Println(fmt.Sprintf("command: %+v", cmd))
+	// AzureBillTrans(cmd)
+	mysqlRule7()
+}
+func mysqlRule7() {
+	result := true
+	message := ""
+	mysqlCnf := "/etc/my.cnf"
+
+	cmd1 := exec.Command("cat", mysqlCnf)
+	cmd2 := exec.Command("grep", "skip-grant-tables")
+	var outbuf1 bytes.Buffer
+	cmd1.Stdout = &outbuf1
+	if err := cmd1.Start(); err != nil {
+		message += err.Error()
+		result = false
+		fmt.Println("message1:", message)
+		fmt.Println("result1:", result)
 		return
 	}
-	fmt.Println(fmt.Sprintf("command: %+v", cmd))
-	AzureBillTrans(cmd)
-}
+	if err := cmd1.Wait(); err != nil {
+		message += err.Error()
+		result = false
+		fmt.Println("message2:", message)
+		fmt.Println("result2:", result)
+		return
+	}
+	var outbuf2 bytes.Buffer
+	cmd2.Stdin = &outbuf1
+	cmd2.Stdout = &outbuf2
+	if err := cmd2.Start(); err != nil {
+		message += err.Error()
+		result = false
+		fmt.Println("message3:", message)
+		fmt.Println("result3:", result)
+		return
+	}
+	if err := cmd2.Wait(); err != nil {
+		message += err.Error()
+		result = false
+		fmt.Println("message4:", message)
+		fmt.Println("result4:", result)
+		return
+	}
+	if outbuf2.String() != "" {
+		result = false
+	}
 
+	fmt.Println("message5:", message)
+	fmt.Println("result5:", result)
+
+	return
+}
 func AzureBillTrans(cmd Command) {
 	defer func() {
 		if e := recover(); e != nil {
